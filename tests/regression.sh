2f54fb843521d0660b88946185d4eaece27d458d
Víctor González Prieto <victor.gp.dev@gmail.com>
2022-03-03 02:29:55 +0100

    Initial commit
    No useful scoping yet. I focused on building a feedback loop with end
    result regression (tests/highlighted/*, tests/regression.sh) and also
    with a syntax_test [1] (tests/syntax/*) for unit testing scope
    assignation.
    
    The later requires syntect (the syntax highlighting library bat uses)
    and its syntest example [2] for running the syntax_test. I didn't commit
    that cause I plan to dockerize it later, once I've gotten somewhere with
    this.
    
    [1] https://www.sublimetext.com/docs/syntax.html#testing
    [2] https://github.com/trishume/syntect/blob/master/examples/syntest.rs

---
+++ tests/regression.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -eou pipefail
+
+# help.sublime-syntax must be symlinked to ~/.config/bat/syntaxes/
+bat cache --build
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+for source_path in source/*
+do
+    filename=$(basename "$source_path")
+    highlighted_path="highlighted/$filename"
+    bat --no-config -fpl help "$source_path" > "$highlighted_path"
+done
+
+# have git tell the effective difference between the version of the syntax
+# in staging/HEAD and the one in working dir, for all highlighted samples
+GIT_PAGER='LESS=R less' git diff --color=never -- highlighted/
