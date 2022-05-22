9c1db91e869d4d459e4da7648e0f4d7a33ae2b13
Víctor González Prieto <victor.gp.dev@gmail.com>
2022-05-22 12:06:24 +0200

    Add nice-to-have task to theme_regression.sh
    and other misc changes.

--- tests/theme_regression.sh
+++ tests/theme_regression.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 
+#nice: add tests for --italic-text=always, removing duplicates
+#      and replace the IFS hack while I'm at it
+
 set -euo pipefail
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 bat cache --build --source .. > /dev/null
 
-highlight() {
-    bat --no-config -fpl cmd-help theme-source/synthetic.txt --theme "$1" > "$2"
-}
+cmd_prefix="bat --no-config -fpl cmd-help theme-source/synthetic.txt"
 
 # this is a hell of a hack :see_no_evil:
 IFS=$'\n'
 for theme in $(bat --list-themes --color=never)
 do
     unset IFS # then bash uses default IFS
-    highlight "$theme" "theme/${theme}.txt"
+    $cmd_prefix --theme="$theme" > "theme/${theme}.txt"
 done
 
 # restore user configuration
 bat cache --build > /dev/null
 
-# have git tell the effective difference between the version of the syntax
-# in staging/HEAD and the one in working dir, for all themes
+# effective difference between HEAD/staging and working dir
 GIT_PAGER='LESS=R less' git diff -- theme/
