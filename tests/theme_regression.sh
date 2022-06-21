a9010bc7b33b953dd242ec543864385675eb7ee6
Víctor González Prieto <victor.gp.dev@gmail.com>
2022-06-21 12:24:00 +0200

    Track italics option in theme regression tests
    A file with the italics off (default) and one with them.
    
    Only keeps an italics file for those themes where the current version of
    the syntax displays italics.

--- tests/theme_regression.sh
+++ tests/theme_regression.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
-#nice: add tests for --italic-text=always, removing duplicates
-#      and replace the IFS hack while I'm at it
-
 set -euo pipefail
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 bat cache --build --source .. > /dev/null
 
+readarray -t themes <<< "$(bat --list-themes --color=never)"
 cmd_prefix="bat --no-config -fpl cmd-help theme-source/synthetic.txt"
 
-# this is a hell of a hack :see_no_evil:
-IFS=$'\n'
-for theme in $(bat --list-themes --color=never)
-do
-    unset IFS # then bash uses default IFS
+for theme in "${themes[@]}"; do
     $cmd_prefix --theme="$theme" > "theme/${theme}.txt"
+    $cmd_prefix --theme="$theme" --italic-text=always > "theme/${theme}-italics.txt"
+    # remove the italics version if there's no difference
+    if diff "theme/${theme}.txt" "theme/${theme}-italics.txt" > /dev/null
+    then
+        rm "theme/${theme}-italics.txt"
+    fi
 done
 
 # restore user configuration
