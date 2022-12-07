b5436bd1174b5aa5bd8335aa1799dcc2ca972c04
Víctor González Prieto <victor.gp.dev@gmail.com>
2022-12-07 01:37:22 +0100

    Simplify inner_theme_regression.sh
    Taking a page out of inner_demo_themes.sh

--- tests/docker/inner_theme_regression.sh
+++ tests/docker/inner_theme_regression.sh
@@ -13,13 +13,14 @@ readarray -t themes <<< "$(bat --list-themes --color=never)"
 for theme in "${themes[@]}"; do
     $cmd_prefix --theme="$theme" $brief_src > "theme/brief-${theme}.txt"
 
-    $cmd_prefix --theme="$theme" \
-        $synthetic_src > "theme/synthetic-${theme}.txt"
-    $cmd_prefix --theme="$theme" --italic-text=always \
-        $synthetic_src > "theme/synthetic-${theme}-italics.txt"
+    synthetic_dest="theme/synthetic-${theme}.txt"
+    synthetic_it_dest="theme/synthetic-${theme}-italics.txt"
+
+    $cmd_prefix --theme="$theme" $synthetic_src > "$synthetic_dest"
+    $cmd_prefix --theme="$theme" --italic-text=always $synthetic_src > "$synthetic_it_dest"
 
     # remove the italics version if there's no difference
-    if diff "theme/synthetic-${theme}-italics.txt" "theme/synthetic-${theme}.txt" > /dev/null
-    then rm "theme/synthetic-${theme}-italics.txt"
+    if diff "$synthetic_it_dest" "$synthetic_dest" > /dev/null
+    then rm "$synthetic_it_dest"
     fi
 done
