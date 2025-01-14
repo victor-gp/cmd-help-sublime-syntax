280cfcbd174d4f669b173c3ba676c90b51d297ac
Víctor González Prieto <victor.gp.dev@gmail.com>
2025-01-14 02:58:55 +0100

    Update demo_themes scripts
    Copy the relevant changes from highlight/theme_regression that we made
    in the improve-testing-scripts branch.

--- scripts/demo_themes.sh
+++ scripts/demo_themes.sh
@@ -2,15 +2,20 @@
 
 set -euo pipefail
 
+# change dir to scripts/
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-echo -n "using docker image "
-docker build --quiet -f ../tests/docker/bat-test.dockerfile -t bat-test ..
+echo "building docker image ..."
+docker build -f ../tests/docker/bat-test.dockerfile -t bat-test .. &> /dev/null
+printf '\e[A\e[K' # clear previous line
+image_id=$(docker image inspect --format "{{.Id}}" bat-test)
+echo "using docker image $image_id"
 
-vol1="$PWD"/../tests/source/:/tests/source:ro
-vol2="$PWD"/../examples/themes/txt/:/tests/destination
+vol_src="$PWD"/../tests/source/:/tests/source:ro
+vol_dest="$PWD"/../examples/themes/txt/:/tests/destination
 
 docker run --rm \
-    -v "$vol1" -v "$vol2" \
-    --entrypoint tests/inner_demo_themes.sh \
+    --user "$(id -u):$(id -g)" \
+    -v "$vol_src" -v "$vol_dest" \
+    --entrypoint /tests/inner_demo_themes.sh \
     bat-test
