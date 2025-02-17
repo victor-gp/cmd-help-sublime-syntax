72833c7c97eae6d6b2893c5935ecade9a29f9933
Víctor González Prieto <victor.gp.dev@gmail.com>
2025-02-17 01:53:28 +0100

    try_input.sh: Add a Docker build step
    To ensure that the bat-test image is available.
    
    Plus $PWD determinism so I can run it from anywhere.

--- scripts/try_input.sh
+++ scripts/try_input.sh
@@ -7,6 +7,17 @@
 #   $ scripts/try_input.sh < <HELP_MSG_FILE>
 #   $ pbpaste | scripts/try_input.sh
 
+set -euo pipefail
+
+# change dir to scripts/
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+echo "building docker image ..."
+docker build -f ../tests/docker/bat-test.dockerfile -t bat-test .. &> /dev/null
+printf '\e[A\e[K' # clear previous line
+image_id=$(docker image inspect --format "{{.Id}}" bat-test)
+echo "using docker image $image_id"
+
 bat_options=(--no-config --plain --language=cmd-help --force-colorization)
 
 cat |
